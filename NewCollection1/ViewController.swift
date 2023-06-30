4fe82fd38b89720b2176841726749498d0bf261b
askerbekovbekzhan <askerbekovbekzhan1@gmail.com>
2023-06-30 09:24:23 +0600

    Initial Commit

---
+++ NewCollection1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewCollection1
+//
+//  Created by Bekzhan Askerbekov on 30/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
